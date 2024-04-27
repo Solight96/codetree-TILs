de30d5c8f2ef7c6fdd7b4186aa6efae3d67c1460
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-04-27 09:55:47 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 153ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240427/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -0,0 +1,40 @@
+N, T = map(int, input().split())
+
+string = str(input())
+
+v = [[0]*N for _ in range(N)]
+check = [[0]*N for _ in range(N)]
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+x, y = N//2, N//2
+
+dir_num = 0
+result = 0
+
+def in_range(x, y):
+    return 0<=x and x<N and 0<=y and y<N
+
+for i in range(N):
+    num = list(map(int, input().split()))
+    v[i] = num
+
+for j in range(len(string)):
+    if check[x][y] == 0:
+        result += v[x][y]
+        check[x][y] = 1
+    if string[j] == 'L':
+        dir_num = (dir_num+3)%4
+    elif string[j] == 'R':
+        dir_num = (dir_num+1)%4
+    elif string[j] == 'F':
+        nx = x + dx[dir_num]
+        ny = y + dy[dir_num]
+        if in_range(nx, ny):
+            x = x + dx[dir_num]
+            y = y + dy[dir_num]
+result += v[x][y]
+
+
+print(result)
