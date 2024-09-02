f86cc45a31a92fc26c867972c10198dfbe830a93
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-09-02 13:47:59 +0900

    Title: 행에 따라 다르게 출력, Time: 138ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240902/행에 따라 다르게 출력/output-differently-for-row.py
@@ -0,0 +1,11 @@
+n = int(input())
+cnt = 0
+
+for i in range(1, n+1):
+    for j in range(n):
+        if i%2 != 0:
+            cnt += 1
+        else:
+            cnt += 2
+        print(cnt, end=' ')
+    print()
