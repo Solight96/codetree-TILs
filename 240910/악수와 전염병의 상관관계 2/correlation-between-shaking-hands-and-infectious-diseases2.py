8e4c96beee7c3e466c8a8ad9f0445f7a6bdfcad4
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-09-10 08:53:56 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240910/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,37 @@
+N,P,K,T = map(int, input().split())
+
+arr_inform = []
+arr_cnt = [0] * (N+1) # (전염된 이후의)악수 횟수
+arr_inj = [0] * (N+1) # 감염 여부
+
+arr_inj[P] = 1
+
+for _ in range(T):
+    inform = input().split()
+    arr_inform.append(inform)
+
+arr_inform.sort()
+
+for i in range(len(arr_inform)):
+    if arr_inj[int(arr_inform[i][1])] == 0:
+        if arr_inj[int(arr_inform[i][2])] == 0:
+            continue
+        else:
+            if arr_cnt[int(arr_inform[i][2])] < K:
+                arr_inj[int(arr_inform[i][1])] = 1
+                arr_cnt[int(arr_inform[i][2])] += 1
+            else:
+                continue
+    else:
+        if arr_inj[int(arr_inform[i][2])] == 1:
+            arr_cnt[int(arr_inform[i][1])] += 1
+        else:
+            if arr_cnt[int(arr_inform[i][1])] < K:
+                arr_inj[int(arr_inform[i][2])] = 1
+                arr_cnt[int(arr_inform[i][1])] += 1
+            else:
+                continue
+
+for i, elem in enumerate(arr_inj):
+    if i >= 1:
+        print(arr_inj[i], end='')
