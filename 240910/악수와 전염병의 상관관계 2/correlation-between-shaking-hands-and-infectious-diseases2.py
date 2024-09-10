40060b758c6e2c50f3ef23d2ff433ad287e1f448
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-09-10 09:35:19 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240910/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240910/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -36,30 +36,3 @@ for i in range(len(arr_inform)):
 for i, elem in enumerate(arr_inj):
     if i >= 1:
         print(arr_inj[i], end='')
-# --------------------------------------------------------------
-# N, K, P, T = map(int, input().split())
-# people = [0] * (N+1)
-# people[P] = 1
-
-# shake = [0] * (N+1)
-# arr = []
-# for i in range(T):
-#     t,x,y = map(int, input().split())
-#     arr.append([t,x,y])
-
-# arr.sort(key=lambda x : x[0])
-
-# for i in range(len(arr)):
-#     if people[arr[i][1]] == 1 and people[arr[i][2]] == 0:
-#         if shake[arr[i][1]] < K:
-#             people[arr[i][2]] = 1
-
-#     if people[arr[i][2]] == 1 and people[arr[i][1]] == 0:
-#         if shake[arr[i][2]] < K:
-#             people[arr[i][1]] = 1
-
-#     shake[arr[i][1]] += 1
-#     shake[arr[i][2]] += 1
-
-# result = people[1:]
-# print(*result, sep='')
