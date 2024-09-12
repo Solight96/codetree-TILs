f39340a60bbc807ed86de2d169ef7b48c60ae50f
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-09-12 09:14:06 +0900

    Title: 되돌아오기, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240912/되돌아오기/come-back.py
@@ -0,0 +1,31 @@
+N = int(input())
+
+x, y = 0, 0
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+dir_num = 0
+t = 0
+result = []
+
+for i in range(1, N+1):
+    direct, dist = map(str, input().split())
+    if direct == 'N':
+        dir_num = 0
+    elif direct == 'E':
+        dir_num = 1
+    if direct == 'S':
+        dir_num = 2
+    if direct == 'W':
+        dir_num = 3
+    for _ in range(int(dist)):
+        x, y = x + dx[dir_num], y + dy[dir_num]
+        t += 1
+        if (x, y) == (0, 0):
+            result.append(t)
+
+if result != []:
+    print(result[0])
+else:
+    print(-1)
