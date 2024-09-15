386efafb6b60882cab848251f324c6814187b2f2
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-09-15 23:26:04 +0900

    Title: 행복한 수열의 개수, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240915/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240915/행복한 수열의 개수/number-of-happy-sequence.py
@@ -2,40 +2,42 @@ n, m = map(int, input().split())
 
 v = [list(map(int, input().split())) for _ in range(n)]
 
-def happyseq(row, col, m):
-    cnt_r = 1
-    cnt_c = 1
-    num = 0
+num = 0
 
-    r=[]
-    c=[]
-    
-    if m == 1:
-        return 2*n
+# 가로
+for i in range(n):
+    number = v[i][0]
+    cnt = 0
 
-    for i in range(row):
-        for j in range(col):
-            if j+1 < col and v[i][j] == v[i][j+1]:
-                cnt_r += 1
-                if cnt_r >= m:
-                    if not i in r:
-                        r.append(i)
-                        num += 1
-            else:
-                cnt_r = 1
-            
-            if i+1 < row and v[i][j] == v[i+1][j]:
-                cnt_c += 1
-                if cnt_c >= m:
-                    if not j in c:
-                        c.append(j)
-                        num += 1
-            else:
-                cnt_c = 1     
-            
-    return num
+     # 갯수에 자기 자신이 포함되므로 자기 자신과 비교할 때부터 1이 카운트 된다
+    for j in range(n):
+        if number == v[i][j]:
+            cnt += 1
+        else:
+           # 다음 숫자와 비교하기 위해 number 에 넣어준다
+            number = v[i][j]
+            cnt = 1
+        if cnt == m:
+            num += 1
+            cnt = 1
+            break
+        
+# 세로
+for j in range(n):
+    number = v[0][j]
+    cnt = 0
 
-result = 0
-result += happyseq(n, n, m)
+    # 갯수에 자기 자신이 포함되므로 자기 자신과 비교할 때부터 1이 카운트 된다
+    for i in range(n):
+        if number == v[i][j]:
+            cnt += 1
+        else:
+            # 다음 숫자와 비교하기 위해 number 에 넣어준다
+            number = v[i][j]
+            cnt = 1
+        if cnt == m:
+            num += 1
+            cnt = 1
+            break
 
-print(result)
+print(num)
