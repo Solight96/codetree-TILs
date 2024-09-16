d8bcd1bfa4827a9f1b24f2a3e7a6aea481330eb5
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-09-16 09:03:30 +0900

    Title: 트로미노, Time: 267ms, Memory: 26MB, Status: Passed - CodeTree

--- 240916/트로미노/tromino.py
+++ 240916/트로미노/tromino.py
@@ -2,21 +2,29 @@ n, m = map(int, input().split())
 
 v = [list(map(int, input().split())) for _ in range(n)]
 
-def rec(v, row_s, row_e, col_s, col_e, n, m):
+def rec_r(row, col_s, col_e):
     num_r = 0
-    num_c = 0
-    for i in range(row_s, row_e+1):
-        if col_e > m:
-            num_r = 0
-        num_c += v[i][col_s]
-    for j in range(col_s, col_e+1):
-        if row_e > n:
-            num_c = 0
-        num_r += v[row_s][j]
     
-    return max(num_r, num_c)
+    for j in range(col_s, col_e+1):
+        if col_e >= m:
+            num_r = 0
+            break
+        num_r += v[row][j]
+    
+    return num_r
 
-def tri(v, row_s, row_e, col_s, col_e):
+def rec_c(row_s, row_e, col):
+    num_c = 0
+    
+    for i in range(row_s, row_e+1):
+        if row_e >= n:
+            num_c = 0
+            break
+        num_c += v[i][col]
+    
+    return num_c
+
+def tri(row_s, row_e, col_s, col_e):
     num = 0
     arr = []
     for i in range(row_s, row_e+1):
@@ -31,9 +39,11 @@ result = 0
 for i in range(n):
     for j in range(m):
         if i+2 < n or j+2 < m:
-            rec_result = rec(v,i,i+2,j,j+2, n, m)
-        if i+1 < n or j+1 < m:
-            tri_result = tri(v,i,i+1,j,j+1)
+            rec_r_result = rec_r(i,j,j+2)
+            rec_c_result = rec_c(i,i+2,j)
+            rec_result = max(rec_r_result, rec_c_result)
+        if i+1 < n and j+1 < m:
+            tri_result = tri(i,i+1,j,j+1)
         result = max(result, max(tri_result, rec_result))
 
 print(result)
