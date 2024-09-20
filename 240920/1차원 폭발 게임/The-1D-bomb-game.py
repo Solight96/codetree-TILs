259617f9a80114b836449b2e0174000880104ae3
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-09-20 23:46:12 +0900

    Title: 1차원 폭발 게임, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240920/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240920/1차원 폭발 게임/The-1D-bomb-game.py
@@ -4,31 +4,35 @@ arr = []
 for _ in range(N):
     arr.append(int(input()))
 
-while True:
-    temp = []
-    index = []
-    cnt = 1
+if M == 1:
+    print(0)
 
-    for i in range(len(arr)-1):
-        if arr[i] == arr[i+1]:
-            cnt += 1
-            if i+1 == len(arr)-1 and cnt >= M:
-                for j in range(i-cnt+2, i+2):
-                    index.append(j)
-        else:
-            if cnt >= M:
-                for j in range(i-cnt+1, i+1):
-                    index.append(j)
-            cnt = 1
-    
-    for i in range(len(arr)):
-        if not i in index:
-            temp.append(arr[i])
-    if temp == arr:
-        break
-    arr = temp
+else:
+    while True:
+        temp = []
+        index = []
+        cnt = 1
 
-print(len(arr))
-    
-for elem in arr:
-    print(elem)
+        for i in range(len(arr)-1):
+            if arr[i] == arr[i+1]:
+                cnt += 1
+                if i+1 == len(arr)-1 and cnt >= M:
+                    for j in range(i-cnt+2, i+2):
+                        index.append(j)
+            else:
+                if cnt >= M:
+                    for j in range(i-cnt+1, i+1):
+                        index.append(j)
+                cnt = 1
+        
+        for i in range(len(arr)):
+            if not i in index:
+                temp.append(arr[i])
+        if temp == arr:
+            break
+        arr = temp
+
+    print(len(arr))
+        
+    for elem in arr:
+        print(elem)
