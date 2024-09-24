a96976f0d3e95dc77e0dadf0a74fceaed6d61063
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-09-25 00:45:36 +0900

    Title: 핀볼게임, Time: 5083ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 240925/핀볼게임/pinball-game.py
@@ -0,0 +1,88 @@
+n = int(input())
+
+v = [list(map(int, input().split())) for _ in range(n)]
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+dxs = [-1,0,1,0]
+dys = [0,1,0,-1]
+
+def pinball(x,y):
+    t = 1
+    arr = []
+    finish = False
+
+    if x == 0:
+        direct = 2
+        while not finish:
+            nx, ny = x+dxs[direct], y+dys[direct]
+            t += 1
+            if in_range(nx, ny):
+                x, y = nx, ny
+                if v[x][y] == 1:
+                    direct = 3
+                elif v[x][y] == 2:
+                    direct = 1
+            else:
+                arr.append(t)
+                finish = True
+            
+    elif x == n-1:
+        direct = 0
+        while not finish:
+            nx, ny = x+dxs[direct], y+dys[direct]
+            t += 1
+            if in_range(nx, ny):
+                x, y = nx, ny
+                if v[x][y] == 1:
+                    direct = 1
+                elif v[x][y] == 2:
+                    direct = 3
+            else:
+                arr.append(t)
+                finish = True
+
+    if y == 0:
+        direct = 1
+        while not finish:
+            nx, ny = x+dxs[direct], y+dys[direct]
+            t += 1
+            if in_range(nx, ny):
+                x, y = nx, ny
+                if v[x][y] == 1:
+                    direct = 0
+                elif v[x][y] == 2:
+                    direct = 2
+            else:
+                arr.append(t)
+                finish = True
+
+    elif y == n-1:
+        direct = 3
+        while not finish:
+            nx, ny = x+dxs[direct], y+dys[direct]
+            t += 1
+            if in_range(nx, ny):
+                x, y = nx, ny
+                if v[x][y] == 1:
+                    direct = 2
+                elif v[x][y] == 2:
+                    direct = 0
+            else:
+                arr.append(t)
+                finish = True
+    else:
+        arr.append(0)
+    
+    return max(arr)
+
+arr2 = []
+
+for i in range(n):
+    for j in range(n):
+        if v[i][j] == 0:
+            result = pinball(i,j)
+            arr2.append(result)
+
+print(max(arr2))
