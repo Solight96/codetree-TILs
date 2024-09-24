5f95331a009d9df4af0d80050ff9d5d3f063ade8
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-09-24 12:17:59 +0900

    Title: 주사위 던지기, Time: 146ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240924/주사위 던지기/roll-a-dice.py
@@ -0,0 +1,45 @@
+n, m, r, c = map(int, input().split())
+r, c = r-1, c-1
+
+v = [[0] * n for _ in range(n)]
+
+direct = list(input().split())
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+dice = [[0,2,0],[4,6,3],[0,5,0]]
+
+drs = [-1,0,1,0]
+dcs = [0,1,0,-1]
+
+i = 0
+v[r][c] = 6
+
+for j in range(m):
+    if direct[j] == 'U':
+        i = 0
+        dice[0][1],dice[1][1],dice[2][1] = dice[1][1],dice[2][1],7-dice[1][1]
+    elif direct[j] == 'R':
+        i = 1
+        dice[1] = [dice[1][1], dice[1][2], 7-dice[1][1]]
+    elif direct[j] == 'D':
+        i = 2
+        dice[0][1],dice[1][1],dice[2][1] = 7-dice[1][1],dice[0][1],dice[1][1]
+    elif direct[j] == 'L':
+        i = 3
+        dice[1] = [7-dice[1][1],dice[1][0],dice[1][1]]
+    
+    nr, nc = r+drs[i], c+dcs[i]
+    
+    if in_range(nr, nc):
+        r, c = nr, nc
+        v[r][c] = dice[1][1]
+
+result = 0
+
+for i in range(n):
+    for j in range(n):
+        result += v[i][j]
+
+print(result)
