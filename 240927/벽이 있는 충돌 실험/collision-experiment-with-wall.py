679754603457cf048c62f1cf07257f234ed8b74d
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-09-27 15:18:15 +0900

    Title: 벽이 있는 충돌 실험, Time: 5092ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 240927/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 240927/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -14,43 +14,36 @@ for _ in range(T):
     v = [[0] * N for _ in range(N)]
     direct = [[-1] * N for _ in range(N)]
 
+    arr = []
+
     for _ in range(M):
         x, y, d = input().split()
         x, y = int(x)-1, int(y)-1
         v[x][y] = 1 # 1은 폭탄
-        if d == 'U': direct[x][y] = 0
-        elif d == 'R': direct[x][y] = 1
-        elif d == 'D': direct[x][y] = 2
-        elif d == 'L': direct[x][y] = 3    
+        if d == 'U': d = 0
+        elif d == 'R': d = 1
+        elif d == 'D': d = 2
+        elif d == 'L': d = 3
+
+        arr.append([x,y,d])    
     
     for _ in range(2*N):
-        v2 = [[0] * N for _ in range(N)]
-        direct2 = [[-1] * N for _ in range(N)]
+        cnt = [[0] * N for _ in range(N)]
+        arr_result = []
 
-        for x in range(N):
-            for y in range(N):
-                if v[x][y] == 1:
-                    nx, ny = x + dxs[direct[x][y]%4], y + dys[direct[x][y]%4]
-                    if in_range(nx, ny):
-                        v2[nx][ny] += 1
-                        direct2[nx][ny] = direct[x][y]
-                    else:
-                        v2[x][y] += 1
-                        direct2[x][y] = direct[x][y] + 2
+        for i in range(len(arr)):
+            nx, ny = arr[i][0] + dxs[arr[i][2]%4], arr[i][1] + dys[arr[i][2]%4]
+            if in_range(nx, ny):
+                arr[i] = [nx,ny,arr[i][2]]
+            else:
+                arr[i] = [arr[i][0],arr[i][1],arr[i][2]+2]
                 
-        for x in range(N):
-            for y in range(N):
-                if v2[x][y] >= 2:
-                    v2[x][y] = 0
-                    direct2[x][y] = -1
-                
-        v = copy.deepcopy(v2)
-        direct = copy.deepcopy(direct2)
-    
-    cnt = 0
-
-    for i in range(N):
-        if 1 in v[i]:
-            cnt += v[i].count(1)
+        for i in range(len(arr)):
+            cnt[arr[i][0]][arr[i][1]] += 1
+        for i in range(len(arr)):
+            if cnt[arr[i][0]][arr[i][1]] == 1:
+                arr_result.append(arr[i])
         
-    print(cnt)
+        arr = copy.deepcopy(arr_result)
+        
+    print(len(arr))
