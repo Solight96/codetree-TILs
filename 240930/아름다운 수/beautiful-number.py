0094c7e44febcf5895b8625664b272e26f628380
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-09-30 12:42:29 +0900

    Title: 아름다운 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240930/아름다운 수/beautiful-number.py
@@ -0,0 +1,48 @@
+n = int(input())
+
+ans = []
+
+def chk_beaunum():
+    cnt = 1
+
+    for i in range(len(ans)):
+        if i == len(ans)-1:
+            if cnt == 1:
+                if ans[i] == 1:
+                    return True
+                else:
+                    return False
+            else:
+                if cnt%ans[i] == 0:
+                    return True
+                else:
+                    return False
+
+        elif ans[i] != ans[i+1]:
+            if cnt%ans[i] == 0:
+                cnt = 1
+                continue
+            else:
+                return False
+        else:
+            cnt += 1
+
+global result
+result = 0
+
+def find_result(num):
+    if num == n+1:
+        is_beau = chk_beaunum()
+        if is_beau:
+            result += 1
+        return
+    
+    for i in range(1, 5):
+        ans.append(i)
+        find_result(num+1)
+        ans.pop()
+
+    print(result)
+
+
+find_result(1)
