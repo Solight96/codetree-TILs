bcd58d9c68e7d7449c6713e4cbac9ce7940b5210
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-01 11:25:21 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241001/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,31 @@
+n, m = map(int, input().split())
+
+board = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def is_snake(x, y):
+    return board[x][y] == 0
+
+dxs = [0, 1]
+dys = [1, 0]
+
+def dfs(x, y):
+    for dx,dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        
+        if in_range(nx, ny) and not visited[nx][ny] and not is_snake(nx, ny):
+            x, y = nx, ny
+            visited[x][y] = True
+            dfs(x, y)
+    
+    if visited[n-1][m-1] == 1:
+        return 1
+    else:
+        return 0
+
+visited[0][0] = True
+result = dfs(0, 0)
+print(result)
