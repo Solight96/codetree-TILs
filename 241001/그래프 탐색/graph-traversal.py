c8e8c3709967971bd5d07ae3a87e3c8059d882b5
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-01 09:57:30 +0900

    Title: 그래프 탐색, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241001/그래프 탐색/graph-traversal.py
+++ 241001/그래프 탐색/graph-traversal.py
@@ -1,20 +1,47 @@
+# N, M = map(int, input().split())
+
+# board = [[0] * N for _ in range(N)]
+# visited = [False] * N
+# arr = []
+
+# for _ in range(M):
+#     x, y = map(int, input().split())
+#     x, y = x-1, y-1
+#     board[x][y] = 1
+#     board[y][x] = 1
+
+# def dfs(v):
+#     visited[v] = True
+
+#     for curr in range(N):
+#         if board[v][curr] == 1 and not visited[curr]:
+#             arr.append(curr)
+#             visited[curr] = True
+#             dfs(curr)
+    
+#     return len(arr)
+
+# result = dfs(0)
+# print(result)
+# ------------------------------------------------
+
 N, M = map(int, input().split())
 
-board = [[0] * N for _ in range(N)]
+board = [[]] * N
 visited = [False] * N
 arr = []
 
 for _ in range(M):
     x, y = map(int, input().split())
     x, y = x-1, y-1
-    board[x][y] = 1
-    board[y][x] = 1
+    board[x].append(y)
+    board[y].append(x)
 
 def dfs(v):
     visited[v] = True
 
-    for curr in range(N):
-        if board[v][curr] == 1 and not visited[curr]:
+    for curr in board[v]:
+        if not visited[curr]:
             arr.append(curr)
             visited[curr] = True
             dfs(curr)
