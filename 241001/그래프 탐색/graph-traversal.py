7f58f2c8fbe9a87a2d2cd82b99de75794881ac97
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-01 09:49:35 +0900

    Title: 그래프 탐색, Time: 208ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 241001/그래프 탐색/graph-traversal.py
@@ -0,0 +1,25 @@
+N, M = map(int, input().split())
+
+board = [[0] * N for _ in range(N)]
+visited = [False] * N
+arr = []
+
+for _ in range(M):
+    x, y = map(int, input().split())
+    x, y = x-1, y-1
+    board[x][y] = 1
+    board[y][x] = 1
+
+def dfs(v):
+    visited[v] = True
+
+    for curr in range(N):
+        if board[v][curr] == 1 and not visited[curr]:
+            arr.append(curr)
+            visited[curr] = True
+            dfs(curr)
+    
+    return len(arr)
+
+result = dfs(0)
+print(result)
