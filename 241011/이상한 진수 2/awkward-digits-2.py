8db68111e05992015623c8a5ea80d555e23d25dd
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-11 11:50:58 +0900

    Title: 이상한 진수 2, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241011/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,16 @@
+a = list(input())
+
+for i in range(1, len(a)):
+    if i == len(a)-1 and a[i] == '1':
+        a[i] = '0'
+
+    elif a[i] == '0':
+        a[i] = '1'
+        break
+
+num = 0
+
+for i in range(len(a)):
+    num = 2 * num + int(a[i])
+
+print(num)
