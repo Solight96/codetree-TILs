f503223c32bce5dc41bfcfa6f1c093474bb3954b
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-11 16:07:26 +0900

    Title: Carry 피하기 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241011/Carry 피하기 2/escaping-carry-2.py
+++ 241011/Carry 피하기 2/escaping-carry-2.py
@@ -5,15 +5,15 @@ for _ in range(n):
     arr.append(int(input()))
 
 arr10000 = [elem // 10000 for elem in arr]
-arr1000 = [(elem // 10000) // 1000 for elem in arr]
+arr1000 = [(elem % 10000) // 1000 for elem in arr]
 arr100 = [(elem % 1000) // 100 for elem in arr]
 arr10 = [(elem % 100) // 10 for elem in arr]
 arr1 = [elem % 10 for elem in arr]
 
 result = 0
 
-for i in range(len(arr)):
-    for j in range(i+1, len(arr)):
+for i in range(len(arr)-2):
+    for j in range(i+1, len(arr)-1):
         for k in range(j+1, len(arr)):
             if arr1000[i] + arr1000[j] + arr1000[k] < 10 and arr100[i] + arr100[j] + arr100[k] < 10 and arr10[i] + arr10[j] + arr10[k] < 10 and arr1[i] + arr1[j] + arr1[k] < 10:
                 num = arr[i] + arr[j] + arr[k]
