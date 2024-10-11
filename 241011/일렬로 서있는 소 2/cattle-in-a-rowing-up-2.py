44c41c93ebeae09dd7d80c5819dca54e077b7151
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-11 09:27:32 +0900

    Title: 일렬로 서있는 소 2, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 241011/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 241011/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -5,9 +5,9 @@ num = 0
 
 for i in range(len(height)):
     for j in range(i+1, len(height)):
-        if height[i] < height[j]:
+        if height[i] <= height[j]:
             for k in range(j+1, len(height)):
-                if height[j] < height[k]:
+                if height[j] <= height[k]:
                     num += 1
 
 print(num)
