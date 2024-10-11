bb3852171b52cee4394e7d38111de2db5bb8101a
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-11 17:07:52 +0900

    Title: 특정 수와 근접한 합, Time: 225ms, Memory: 26MB, Status: Passed - CodeTree

--- 241011/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 241011/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,15 +1,19 @@
 import sys
+import copy
 
 N, S = map(int, input().split())
 
 arr = list(map(int, input().split()))
+arr2 = copy.deepcopy(arr)
 
 result = sys.maxsize
 
 for i in range(len(arr)-1):
-    arr[i], arr[i+1] = 0, 0
-    dist = abs(S - sum(arr))
-    if result > dist:
-        result = dist
+    for j in range(i+1, len(arr)):
+        arr2[i], arr2[j] = 0, 0
+        dist = abs(S - sum(arr2))
+        if result > dist:
+            result = dist
+        arr2 = copy.deepcopy(arr)
 
 print(result)
