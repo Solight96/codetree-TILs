368e3925b810e21fe6dc3e4e17cfd55eb2c3ca07
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-14 08:09:00 +0900

    Title: 아름다운 수열 2, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 241014/아름다운 수열 2/beautiful-sequence-2.py
+++ 241014/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,3 +1,5 @@
+import copy
+
 N, M = map(int, input().split())
 
 A = list(map(int, input().split()))
@@ -6,15 +8,11 @@ B = list(map(int, input().split()))
 num = 0
 
 for i in range(N-M+1):
-    C = [0] * M
-    cnt = 0
+    C = copy.deepcopy(B)
     for j in range(i, i+M):
-        if A[j] in B:
-            C[B.index(A[j])] += 1
-    for k in range(M):
-        if C[k] >= 1:
-            cnt += 1
-    if cnt == M:
+        if A[j] in C:
+            C.pop(C.index(A[j]))
+    if not len(C):
         num += 1
 
 print(num)
