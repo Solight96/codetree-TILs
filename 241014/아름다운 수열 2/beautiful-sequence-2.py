d3286fa7396db73a86fc298ee1adce1660c3e1fc
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-14 07:58:38 +0900

    Title: 아름다운 수열 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241014/아름다운 수열 2/beautiful-sequence-2.py
+++ 241014/아름다운 수열 2/beautiful-sequence-2.py
@@ -6,9 +6,13 @@ B = list(map(int, input().split()))
 num = 0
 
 for i in range(N-M+1):
+    C = [0] * M
     cnt = 0
     for j in range(i, i+M):
         if A[j] in B:
+            C[B.index(A[j])] += 1
+    for k in range(M):
+        if C[k] >= 1:
             cnt += 1
     if cnt == M:
         num += 1
