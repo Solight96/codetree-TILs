2626ee6633901c8ad1457bf121acf622a68e9014
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-14 09:27:03 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 241014/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 241014/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -7,8 +7,10 @@ for i in range(len(string)):
         num += 1
     else:
         num -= 1
-
-if string[0] == '(' and num == 0:
-    print('Yes')
-else:
-    print('No')
+    if num < 0:
+        print('No')
+        break
+    if i == len(string) -1 and num == 0:
+        print('Yes')
+    elif i == len(string) -1 and num != 0:
+        print('No')
