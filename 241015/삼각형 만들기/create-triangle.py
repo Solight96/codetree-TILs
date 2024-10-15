6e57bd17a2ad6db1b5b991234452a2488a602fe3
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-15 23:05:54 +0900

    Title: 삼각형 만들기, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241015/삼각형 만들기/create-triangle.py
+++ 241015/삼각형 만들기/create-triangle.py
@@ -9,6 +9,7 @@ result = 0
 
 for i in range(len(arr)-2):
     for j in range(i+1, len(arr)-1):
+        num = 0
         if arr[i][0] == arr[j][0]:
             for k in range(j+1, len(arr)):
                 if arr[i][1] == arr[k][1] or arr[j][1] == arr[k][1]:
