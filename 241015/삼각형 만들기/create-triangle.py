d30dee1720e55aa50b08af80364ec760f3171602
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-10-15 23:18:24 +0900

    Title: 삼각형 만들기, Time: 216ms, Memory: 26MB, Status: Passed - CodeTree

--- 241015/삼각형 만들기/create-triangle.py
+++ 241015/삼각형 만들기/create-triangle.py
@@ -13,18 +13,20 @@ for i in range(len(arr)-2):
         if arr[i][0] == arr[j][0]:
             for k in range(j+1, len(arr)):
                 if arr[i][1] == arr[k][1] or arr[j][1] == arr[k][1]:
-                    num = abs(arr[i][1]-arr[j][1]) * abs(arr[i][0]-arr[k][0])
+                    num = abs(arr[i][1]-arr[j][1]) * abs(arr[j][0]-arr[k][0])
+                    result = max(result, num)
         elif arr[i][1] == arr[j][1]:
             for k in range(j+1, len(arr)):
                 if arr[i][0] == arr[k][0] or arr[j][0] == arr[k][0]:
-                    num = abs(arr[i][0]-arr[j][0]) * abs(arr[i][1]-arr[k][1])
+                    num = abs(arr[i][0]-arr[j][0]) * abs(arr[j][1]-arr[k][1])
+                    result = max(result, num)
         else:
             for k in range(j+1, len(arr)):
                 if arr[i][0] == arr[k][0] and arr[j][1] == arr[k][1]:
                     num = abs(arr[i][1]-arr[k][1]) * abs(arr[j][0]-arr[k][0])
+                    result = max(result, num)
                 elif arr[i][1] == arr[k][1] and arr[j][0] == arr[k][0]:
                     num = abs(arr[i][0]-arr[k][0]) * abs(arr[j][1]-arr[k][1])
-    
-        result = max(result, num)
+                    result = max(result, num)
 
 print(result)
