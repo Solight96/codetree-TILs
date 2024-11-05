259551cd6e2b08a51edcf5642bcee135731ca085
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-11-05 23:03:03 +0900

    Title: 빙산의 일각 2, Time: 87ms, Memory: 19MB, Status: Wrong Answer - CodeTree

---
+++ 241105/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -0,0 +1,26 @@
+import copy
+
+N = int(input())
+
+arr = []
+
+for _ in range(N):
+    arr.append(int(input()))
+
+result = []
+
+for h in range(max(arr)):
+    arr2 = copy.deepcopy(arr)
+    for i in range(len(arr2)):
+        if arr2[i] <= h:
+            arr2[i] = 0
+    
+    cnt = 0
+
+    for i in range(len(arr2)-1):
+        if arr2[i] != 0 and arr2[i+1] == 0:
+            cnt += 1
+    
+    result.append(cnt+1)
+
+print(max(result))
