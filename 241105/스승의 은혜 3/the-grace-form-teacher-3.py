af9acca779e72e9aff8f403832035e54bf6c0428
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-11-05 11:08:08 +0900

    Title: 스승의 은혜 3, Time: 71ms, Memory: 17MB, Status: Passed - CodeTree

--- 241105/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 241105/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -9,13 +9,14 @@ for _ in range(N):
 
 for i in range(N):
     cnt = 0
+    B2 = copy.deepcopy(B)
     arr2 = copy.deepcopy(arr)
     arr2[i][0] //= 2
     arr2.sort(key=lambda x: x[0]+x[1])
     
     for j in range(len(arr2)):
-        B -= (arr2[j][0] + arr2[j][1])
-        if B >= 0:
+        B2 -= (arr2[j][0] + arr2[j][1])
+        if B2 >= 0:
             cnt += 1
         else:
             student.append(cnt)
