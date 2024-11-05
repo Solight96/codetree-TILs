e731682507fa79bb3cfafcc1e36c112b88e22acc
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-11-05 14:56:02 +0900

    Title: 상해버린 치즈, Time: 58ms, Memory: 16MB, Status: Passed - CodeTree

--- 241105/상해버린 치즈/rotten-cheese.py
+++ 241105/상해버린 치즈/rotten-cheese.py
@@ -11,31 +11,37 @@ for j in range(S):
     arr_s.append(list(map(int, input().split()))) # p(누가), t(언제 아팠니)
 
 arr_pmt = []
+arr_people = []
 
 for m in range(S):
     for n in range(D):
         if arr_s[m][0] == arr_d[n][0] and arr_s[m][1] > arr_d[n][2]: # 치즈를 먹고나서 아프게 됐으면
             arr_pmt.append(arr_d[n]) # 그 때의 pmt를 저장 (아프기 전에 먹었던 치즈 정보 확보)
+            arr_people.append(arr_d[n][0])
+arr_pmt.sort()
+num_people = len(set(arr_people))
 
-arr_m = []
+arr_m = [[] for _ in range(N+1)]
+arr_m2 = []
 
-if len(arr_pmt) != 1 and S != 1: # 아픈 사람이 여러 명이면
-    for i in range(len(arr_pmt)):
-        for j in range(len(arr_pmt)):
-            if i == j:
-                continue
-            if arr_pmt[i][1] == arr_pmt[j][1]: # 같은 치즈를 먹었으면
-                if not arr_pmt[j][1] in arr_m:
-                    arr_m.append(arr_pmt[j][1]) # 그 때의 m을 저장 (같이 먹었기에 의심되는 치즈..)
-else: # 아픈 사람이 혼자면
-    arr_m = [arr_pmt[0][1]]
+for i in range(len(arr_pmt)):
+    p,m,t = arr_pmt[i]
+    arr_m[p].append(m)
+
+for i in range(M):
+    cnt = 0
+    for j in range(len(arr_m)):
+        if i in arr_m[j]:
+            cnt += 1
+    if cnt == num_people:
+        arr_m2.append(i)
 
 arr = []
 
-for i in range(len(arr_m)):
+for i in range(len(arr_m2)):
     arr_p = []
     for j in range(D):
-        if arr_m[i] == arr_d[j][1]: # 의심되는 치즈를 먹은 사람
+        if arr_m2[i] == arr_d[j][1]: # 의심되는 치즈를 먹은 사람
             if not arr_d[j][0] in arr_p:
                 arr_p.append(arr_d[j][0])
     arr.append(len(arr_p))
