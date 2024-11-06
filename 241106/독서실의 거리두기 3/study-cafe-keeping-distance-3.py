9e1fe7568f339a824e80b7e7083c80e7186feb45
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-11-06 13:08:19 +0900

    Title: 독서실의 거리두기 3, Time: 49ms, Memory: 15MB, Status: Passed - CodeTree

--- 241106/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 241106/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -21,4 +21,22 @@ for i in range(len(arr)-1):
         y = j
 
 arr[(x+y)//2] = '1'
-print(min(abs(x-(x+y)//2), abs(y-(x+y)//2)))
+
+result2 = 1000000
+
+for i in range(len(arr)-1):
+    if arr[i] == '1':
+        cnt = 0
+        for j in range(i+1, len(arr)):
+            if not arr[j] == '1':
+                cnt += 1
+            else:
+                cnt += 1
+                break
+    
+    if result2 > cnt:
+        result2 = cnt
+
+print(result2)
+
+# print(min(abs(x-(x+y)//2), abs(y-(x+y)//2)))
