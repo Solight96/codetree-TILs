ada437a063cd9eecaefdb7ab77f27601bbf7f0fe
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-11-06 10:53:16 +0900

    Title: 연속된 숫자 만들기 2, Time: 42ms, Memory: 15MB, Status: Passed - CodeTree

--- 241106/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 241106/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -10,23 +10,35 @@ cnt = 0
 while not (d1 == 1 and d2 == 1):
     if d1 <= d2:
         if not d1 == 1:
-            tmp = d1
-            d1 = d1 // 2
-            d2 = tmp - d1
+            if d1 > 2:
+                d2 = 2
+                d1 -= d2
+            else:
+                d2 = 1
+                d1 -= d2
         else:
-            tmp = d2
-            d2 = d2 // 2
-            d1 = tmp - d2
+            if d2 > 2:
+                d1 = 2
+                d2 -= d1
+            else:
+                d1 = 1
+                d2 -= d1
         cnt += 1
     else:
         if not d2 == 1:
-            tmp = d2
-            d2 = d2 // 2
-            d1 = tmp - d2
+            if d2 > 2:
+                d1 = 2
+                d2 -= d1
+            else:
+                d1 = 1
+                d2 -= d1
         else:
-            tmp = d1
-            d1 = d1 // 2
-            d2 = tmp - d1
+            if d1 > 2:
+                d2 = 2
+                d1 -= d2
+            else:
+                d2 = 1
+                d1 -= d2
         cnt += 1
 
 print(cnt)
