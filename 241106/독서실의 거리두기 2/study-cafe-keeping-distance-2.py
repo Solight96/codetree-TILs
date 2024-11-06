d85e49256c5cd95d7424a1f0f8af26243cf4f2a7
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-11-06 23:57:22 +0900

    Title: 독서실의 거리두기 2, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241106/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 241106/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -3,48 +3,48 @@ N = int(input())
 arr = list(input())
 
 num = 0
-x, y, = 0, 0
+x, y = 0, 0
+num_list = []
 
-for i in range(len(arr)-1):
-    cnt = 0
-    if arr[i] == '1':
-        for j in range(i+1, len(arr)):
-            if arr[j] != '1':
-                cnt += 1
-            else:
-                cnt += 1
-                break
-    if cnt != 0:
-        cnt_c = cnt
-    if num < cnt_c:
-        num = cnt_c
-        x = i
-        y = j
+for i in range(len(arr)):
+    if arr[i] == '0':
+        arr[i] = '1'
+        for j in range(len(arr)-1):
+            if arr[j] == '1':
+                cnt = 0
+                for k in range(j+1, len(arr)):
+                    if arr[k] == '0':
+                        cnt += 1
+                    else:
+                        cnt += 1
+                        break
+        print(arr)
+        print(cnt)
+        if num < cnt:
+            num = cnt
+            x = j
+            y = k
 
-if arr[-1] == '1':
-    arr[(x+y)//2] = '1'
-else:
-    if cnt_c < y - (x+y)//2 or cnt_c < (x+y)//2 - x:
-        arr[(x+y)//2] = '1'
-    else:
-        arr[-1] = '1'
+    num_list.append((num, j, k))
 
-num2 = 1000000
+print(num_list)
 
-for i in range(len(arr)-1):
-    if arr[i] == '1':
-        cnt = 0
-        for j in range(i+1, len(arr)):
-            if j == len(arr)-1 and arr[j] == '0':
-                cnt = 1000000
-            else:
-                if arr[j] != '1':
-                    cnt += 1
-                else:
-                    cnt += 1
-                    break
+# num2 = 1000000
 
-        if num2 > cnt:
-            num2 = cnt
+# for i in range(len(arr)-1):
+#     if arr[i] == '1':
+#         cnt = 0
+#         for j in range(i+1, len(arr)):
+#             if j == len(arr)-1 and arr[j] == '0':
+#                 cnt = 1000000
+#             else:
+#                 if arr[j] != '1':
+#                     cnt += 1
+#                 else:
+#                     cnt += 1
+#                     break
 
-print(num2)
+#         if num2 > cnt:
+#             num2 = cnt
+
+# print(num2)
