23f3a7139cf9da6202fb39017646b920128fd840
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-11-07 22:09:21 +0900

    Title: 거꾸로 순열, Time: 169ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241107/거꾸로 순열/backward-permutation.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+arr = []
+visited = [0] * (n+1)
+
+def choose(idx):
+    if idx == n+1:
+        for elem in arr:
+            print(elem, end=' ')
+        print()
+        return
+    
+    for i in range(n, 0, -1):
+        if visited[i]:
+            continue
+        
+        visited[i] = 1
+        arr.append(i)
+        choose(idx+1)
+
+        arr.pop()
+        visited[i] = 0
+
+choose(1)
