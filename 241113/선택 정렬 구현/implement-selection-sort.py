4fe48d4dbbd5e82d38d709efe65f36a8aca1a17a
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-11-13 22:18:32 +0900

    Title: 선택 정렬 구현, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241113/선택 정렬 구현/implement-selection-sort.py
+++ 241113/선택 정렬 구현/implement-selection-sort.py
@@ -1,19 +1,14 @@
-import sys
-
 n = int(input())
 
 arr = list(map(int, input().split()))
 
-
 for i in range(len(arr)-1):
-    minimum = sys.maxsize
-    for j in range(i, len(arr)):
-        if minimum > arr[j]:
-            minimum = arr[j]
-    
-    idx = arr.index(minimum)
-    tmp = arr[i]
-    arr[i] = arr[idx]
-    arr[idx] = tmp
+    minimum = i
+    for j in range(i+1, len(arr)):
+        if arr[minimum] > arr[j]:
+            minimum = j
+    tmp = arr[minimum]
+    arr[minimum] = arr[i]
+    arr[i] = tmp
 
 print(*arr)
