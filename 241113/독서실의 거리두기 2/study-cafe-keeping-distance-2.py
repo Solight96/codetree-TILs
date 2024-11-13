864bac742d4092045a8e344046fe7d631591cd00
Solight96 <58461685+Solight96@users.noreply.github.com>
2024-11-13 21:43:42 +0900

    Title: 독서실의 거리두기 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241113/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,57 @@
+N = int(input())
+
+arr = list(input())
+
+num = 0
+x = 0
+num_list = []
+
+for i in range(len(arr)):
+    if arr[i] == '0':
+        arr[i] = '1'
+        cnt_list = []
+        for j in range(len(arr)-1):
+            if arr[j] == '1':
+                cnt = 0
+                for k in range(j+1, len(arr)):
+                    if arr[k] == '0':
+                        if k == len(arr) - 1:
+                            break
+                        else:
+                            cnt += 1
+                    else:
+                        cnt += 1
+                        break
+                if cnt != 0:
+                    cnt_list.append(cnt)
+            
+        cnt = min(cnt_list)
+        if num < cnt:
+            num = cnt
+            x = i
+        num_list.append((num, x))
+        arr[i] = '0'
+
+num_list.sort()
+x = num_list[-1][1]
+arr[x] = '1'
+
+num2 = 1000000
+
+for i in range(len(arr)-1):
+    if arr[i] == '1':
+        cnt = 0
+        for j in range(i+1, len(arr)):
+            if j == len(arr)-1 and arr[j] == '0':
+                cnt = 1000000
+            else:
+                if arr[j] != '1':
+                    cnt += 1
+                else:
+                    cnt += 1
+                    break
+
+        if num2 > cnt:
+            num2 = cnt
+
+print(num2)
