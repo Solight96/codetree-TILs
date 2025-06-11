44a222d2f33888c851e8e2c544a2db277f2e0c9c
Solight96 <58461685+Solight96@users.noreply.github.com>
2025-06-11 20:13:44 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250611/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 250611/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -4,12 +4,16 @@ b = list(map(int, input().split()))
 
 def is_conti_partial_seq(a, b):
     q = b[0]
+
     for i in range(len(a)):
+        if i+len(b) > len(a): return False
         if a[i] == q:
-            if i+len(b) > len(a): return False
+            s = 1
             for j in range(i, i+len(b)):
-                if a[j] != b[j-i]: return False
-            return True
+                if a[j] != b[j-i]:
+                    s = 0
+                    break
+            if s: return True
 
 if is_conti_partial_seq(a,b):
     print("Yes")
